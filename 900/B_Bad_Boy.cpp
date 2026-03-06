8c0ed18ae0621e308fc7f3ac1e3a7a0994be7a44
AkshatVarshney10 <akshatvarshney04@gmail.com>
2026-03-06 22:40:42 +0530

    1537B

---
+++ 900/B_Bad_Boy.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+void solve() {
+    int n,m,i,j;
+    cin>>n>>m>>i>>j;
+    cout<<1<<" "<<1<<" "<<n<<" "<<m<<endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
