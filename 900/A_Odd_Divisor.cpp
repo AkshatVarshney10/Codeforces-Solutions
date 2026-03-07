60a35ab644b3f3eb7fe5f9d4b68c7726983ca125
AkshatVarshney10 <akshatvarshney04@gmail.com>
2026-03-07 23:31:24 +0530

    1475A

---
+++ 900/A_Odd_Divisor.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+void solve() {
+    ll n;
+    cin>>n;
+    while(n%2==0){
+        n = n/2;
+    }
+    if(n>1){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
