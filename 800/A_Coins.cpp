bc64ec6ffaec0a16d81832ae135c7b38a476c1bb
AkshatVarshney10 <akshatvarshney04@gmail.com>
2026-03-08 16:33:34 +0530

    1814A

--- 800/A_Coins.cpp
+++ 800/A_Coins.cpp
@@ -1,17 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n,k;
-        cin>>n>>k;
-        if(n%2==0||(n-k)%2==0){
-            cout<<"YES"<<endl;
-        }
-        else{
-            cout<<"NO"<<endl;
-        }
+typedef long long ll;
+
+void solve() {
+    ll n,k;
+    cin>>n>>k;
+    if(n%2==0||(n-k)%2==0){
+        cout<<"YES"<<endl;
     }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
 }
